4a12bf81bf22ed2b63a020aabcbc74c1b4f95090
ToriMun <132326048+torimun@users.noreply.github.com>
2023-05-29 09:14:30 +0000

    [Add]アプリケーションを完成２させよう　10章まで学習完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    #1:N の「1」側にあたるモデルに、has_many を記載する必要がある
+    has_many :post_iamges, dependent: :destroy
 end
